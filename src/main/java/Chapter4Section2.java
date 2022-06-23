42e5b92784cf69ffc2bbba0495fe19c494e2e72b
jhdl0157 <jhdl0157@naver.com>
2022-06-23 23:38:43 +0900

    컨슈머를 활용한 프로세싱

---
+++ src/main/java/Chapter4Section2.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Chapter4Section2 {
+    public static void main(String[] args) {
+        //서플라이어와 다르게 주어진걸 먹어치운다.
+        Consumer<String> myStringConsumer= System.out::println;
+        myStringConsumer.accept("냠냠냠 이거 다 먹는다.");
+
+        List<Integer> integerInputs= Arrays.asList(4,2,3); //불변 객체이다.
+        //integerInputs.add(4); 불가능
+        Consumer<Integer> myIntegerProcessor = (x)->{
+            System.out.println("프로세싱중: "+x);
+        };
+        process(integerInputs,myIntegerProcessor);
+        Consumer<Integer> myDifferentInteger = x-> System.out.println("다른 프로세스 진행중 "+x);
+        process(integerInputs,myDifferentInteger);
+    }
+//컨슈머 프로세스 로직에 따라서 다양하게 활용이 가능한다.
+    public static <T> void process(List<T> inputs, Consumer<T> processor){
+      for(T input :inputs){
+          processor.accept(input);
+      }
+
+    }
+}
--- src/main/java/ChapterSection4.java
+++ src/main/java/ChapterSection4.java
@@ -2,14 +2,13 @@ import java.util.function.Supplier;
 
 public class ChapterSection4 {
     public static void main(String[] args) {
+        //서플라이어는 무언가를 받으면 리턴만 해준다.
         Supplier<String> myStringSupplier=()->"hello world";
         System.out.println(myStringSupplier.get());
         Supplier<Double> myRandomSupplier=()-> Math.random();
-        System.out.println(myRandomSupplier.get());
-        System.out.println(myRandomSupplier.get());
-        System.out.println(myRandomSupplier.get());
-
         printRandomDoubles(myRandomSupplier,10);
+
+
     }
 //이 함수는 랜덤 로직에 전혀 영향을 안받는다. 그저 출력만 할뿐
     public static void printRandomDoubles(Supplier<Double> randomSupplier,int count){
