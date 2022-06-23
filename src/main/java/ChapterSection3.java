5310dfe347bb83f54aae73950d72ac65a67ae4e0
jhdl0157 <jhdl0157@naver.com>
2022-06-23 23:20:16 +0900

    TriFunction

--- src/main/java/ChapterSection3.java
+++ src/main/java/ChapterSection3.java
@@ -1,3 +1,5 @@
+import util.TriFunction;
+
 import java.util.function.BiFunction;
 
 public class ChapterSection3 {
@@ -9,8 +11,12 @@ public class ChapterSection3 {
         int result=add.apply(3,5);
         System.out.println(result);
         //매개변수 인풋타입은 유추 가능하니 생략
-        BiFunction<Integer,Integer,Integer> add1=( x,  y) -> x+y;
+        BiFunction<Integer,Integer,Integer> add1=(x,y) -> x+y;
         int result1=add1.apply(3,5);
         System.out.println(result1);
+        //3개를 받는 인터페이스는 없다 직접 만들어보자
+        TriFunction<Integer,Integer,Integer,Integer> addThree=( x, y, z)->x+y+z;
+        int result2=addThree.apply(3,2,5);
+        System.out.println(result2);
     }
 }
