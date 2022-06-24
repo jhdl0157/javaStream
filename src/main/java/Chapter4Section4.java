3a2c77199d57392529f0d234baec76341cef5eff
jhdl0157 <jhdl0157@naver.com>
2022-06-25 00:16:40 +0900

    Predicate 심화

--- src/main/java/Chapter4Section4.java
+++ src/main/java/Chapter4Section4.java
@@ -7,9 +7,13 @@ public class Chapter4Section4 {
     public static void main(String[] args) {
         Predicate<Integer> isPositive=x-> x>0;
         System.out.println(isPositive.test(10));
-        List<Integer> inputs= Arrays.asList(10,-5,4,-2,0);
+        List<Integer> inputs= Arrays.asList(10,-5,4,-2,0,3);
         System.out.println("Positive number "+filter(inputs,isPositive));
-
+        System.out.println("Non Positive number"+filter(inputs,isPositive.negate()));
+        System.out.println(
+                "Non-negative number "+
+                        filter(inputs,isPositive.or(x->x==0)));
+        System.out.println("Positive even number "+filter(inputs,isPositive.and(x->x%2==0)));
     }
     public static <T> List<T> filter(List<T> inputs,Predicate<T> condition){
         List<T> output=new ArrayList<>();
