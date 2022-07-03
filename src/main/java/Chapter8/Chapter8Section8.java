bb584d537454c0a9dd08b15d9c4e67aeb169176b
jhdl0157 <jhdl0157@naver.com>
2022-07-03 20:50:27 +0900

    UserPartitioningBy 적용

--- src/main/java/Chapter8/Chapter8Section8.java
+++ src/main/java/Chapter8/Chapter8Section8.java
@@ -1,6 +1,9 @@
 package Chapter8;
 
 
+import Chapter8.Service.EmailService;
+import Chapter8.model.User;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -13,5 +16,40 @@ public class Chapter8Section8 {
                 .collect(Collectors.partitioningBy(number->number%2==0));
         System.out.println("Even number :"+numberPartitions.get(true));
         System.out.println("Odd number :"+numberPartitions.get(false));
+
+
+        User user1 = new User()
+                .setId(101)
+                .setName("Paul")
+                .setVerified(true)
+                .setEmailAddress("alice@fastcampus.co.kr")
+                .setFriendUserIds(Arrays.asList(201,202,203,204,211,212,213,214));
+
+        User user2 = new User()
+                .setId(102)
+                .setName("David")
+                .setVerified(true)
+                .setEmailAddress("bob@fastcampus.co.kr")
+                .setFriendUserIds(Arrays.asList(204,205,206));
+
+        User user3 = new User()
+                .setId(103)
+                .setName("John")
+                .setVerified(true)
+                .setEmailAddress("Charlie@fastcampus.co.kr")
+                .setFriendUserIds(Arrays.asList(204,205,207,208));
+        List<User> users = Arrays.asList(user1, user2, user3);
+        Map<Boolean,List<User>> userPartitions=users.stream()
+                .collect(Collectors.partitioningBy(user->user.getFriendUserIds().size()>5));
+
+        EmailService emailService=new EmailService();
+        for(User user:userPartitions.get(true)){
+            emailService.sendPlayWithFriendsEmail(user);
+        }
+        for(User user:userPartitions.get(false)){
+            emailService.sendMakeMoreFriendsEmail(user);
+        }
+
+
     }
 }
